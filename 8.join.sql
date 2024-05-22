f076ce6ac034718f0995d6e0fa479f3cc7e8ce05
qkdrmawll <eunji8106@naver.com>
2024-05-22 16:43:06 +0900

    데이터베이스 심화

--- 8.join.sql
+++ 8.join.sql
@@ -16,3 +16,52 @@ select p.id, p.title , p.title, a.email  from post p left join author a on p.aut
 select p.title, a.email from post p inner join author a on p.author_id=a.id WHERE a.age >=25;
 -- 2) 모든 글 목록 중에 글의 title과 저자가 있다면 email을 출력, 이때 2024-05-01 이후에 만들어진 글만 출력
 select p.title, a.email FROM post p left join author a on p.author_id  = a.id WHERE p.created_time > '2024-05-01';
+select p.title, a.email FROM post p left join author a on p.author_id  = a.id where DATE_FORMAT(p.created_time) > '2024-05-01';
+
+-- 글을 안 쓴 글쓴이 출력
+select a.id, a.email from author a left join post p on p.author_id=a.id where p.author_id is null;
+
+-- union : 중복제외한 두 테이블의 select을 결합
+-- 컬럼의 개수와 타입이 같아야 함에 유의
+-- union all : 중복 포함
+select 컬럼1, 컬럼2 from table1 union select 컬럼1, 컬럼2 from table2;
+-- author table의 name, email 그리고 post 테이블의 title, contents
+select name, email from author union select title, contents from post;
+
+-- 서브쿼리 : select문 안에 또 다른 select문을 서브쿼리라 한다
+-- select절 안에 서브쿼리
+-- author email과 해당 author가 쓴 글의 개수를 출력
+select email, (select count(*)from post p where p.author_id = a.id) as count from author a ;
+select a.id, if(p.id is null,0,count(*)) from author a left join post p on a.id=p.author_id group by a.id;
+
+-- from절 안에 서브쿼리
+select a.name from (select * from author) as a;
+
+-- where절 안에 서브쿼리
+-- 글을 쓴 글쓴이만 출력
+select a.* from author a inner join post p on a.id = p.author_id;
+select * from author where id in (select author_id from post);
+
+-- 집계함수
+select COUNT(*) from author;
+select sum(price) from post;
+select avg(price) from post;
+select round(avg(price),0) from post;
+
+-- group by와 집계함수
+select author_id, count(*) from post group by author_id;
+
+-- where와 group by
+-- 연도별 post 글 출력, 연도가 null인 데이터는 제외
+select date_format(created_time,'%Y') as year ,count(*) from post p where created_time is not null group by year;
+
+-- HAVING : group by를 통해 나온 통계에 대한 조건
+select count(*) from post group by author_id;
+select count(*) as count from post group by author_id having count>=2;
+--(실습) 포스팅 price가 2000원 이상인 글을 대상으로, 작성자별로 몇건인지와 평균 price를 구하되 평균price가 3000원 이상인 데이터를 대상으로만 통계 출력
+select author_id , count(*), avg(price) as ap from post where price>=2000 group by author_id having ap>=3000;
+-- (실습) 1건 이상의 글을 쓴 사람의 id와 name을 구할건데, 나이는 25세 이상인 사람만 통계에 사용하고, 가장 나이가 많은 사람 1명의 통계만 출력
+select a.id, a.name from post p inner join author a group by author_id having count(*)>=2;
+
+-- 다중열 group by
+select author_id, title, count(title) from post group by author_id, title;
---
+++ dumpfile.sql
@@ -0,0 +1,101 @@
+-- MariaDB dump 10.19-11.3.2-MariaDB, for debian-linux-gnu (aarch64)
+--
+-- Host: localhost    Database: board
+-- ------------------------------------------------------
+-- Server version	11.3.2-MariaDB-1:11.3.2+maria~ubu2204
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `author`
+--
+
+DROP TABLE IF EXISTS `author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `author` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `age` tinyint(3) unsigned DEFAULT NULL,
+  `profile_image` longblob DEFAULT NULL,
+  `role` enum('admin','user') NOT NULL DEFAULT 'user',
+  `birth_day` date DEFAULT NULL,
+  `created_time` datetime DEFAULT current_timestamp(),
+  `post_count` int(11) DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `author`
+--
+
+LOCK TABLES `author` WRITE;
+/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+INSERT INTO `author` VALUES
+(1,NULL,'hi@naver.com',NULL,NULL,10,NULL,'user',NULL,'2024-05-20 02:15:21',0),
+(2,NULL,'hihi@naver.com',NULL,NULL,30,NULL,'user',NULL,'2024-05-20 02:15:40',0),
+(3,'eunji','hihi3@naver.com',NULL,NULL,40,NULL,'user',NULL,'2024-05-20 02:16:15',0),
+(4,'방은지','hello2@naver.com',NULL,NULL,25,NULL,'user',NULL,NULL,0),
+(5,NULL,'eunji@naver.com',NULL,NULL,30,NULL,'user',NULL,NULL,0);
+/*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `post` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `contents` varchar(3000) DEFAULT NULL,
+  `author_id` bigint(20) DEFAULT NULL,
+  `price` decimal(10,3) DEFAULT NULL,
+  `created_time` datetime DEFAULT current_timestamp(),
+  `user_id` char(36) DEFAULT uuid(),
+  PRIMARY KEY (`id`),
+  KEY `author_id` (`author_id`),
+  CONSTRAINT `post_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post`
+--
+
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+INSERT INTO `post` VALUES
+(1,'hello','hello spring',4,1000.000,'2000-01-01 12:00:00','1f211a45-13a1-11ef-b61e-0242ac110002'),
+(2,'hello','content',5,500.000,'2009-01-01 12:00:00','1f212491-13a1-11ef-b61e-0242ac110002'),
+(3,'eunji',NULL,4,30000.000,'2024-05-20 08:03:37','a40a0753-1478-11ef-b61e-0242ac110002'),
+(4,'title1','contents',5,40000.000,'2000-01-01 12:00:00','1f2124ca-13a1-11ef-b61e-0242ac110002');
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-22  7:39:40
